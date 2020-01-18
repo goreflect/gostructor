ecbf57146be4297e31d9b2f98471867d197da0f4
kubitre <kubitre@gmail.com>
2020-01-19 03:13:00 +0700

    create external interface

---
+++ lib.go
@@ -0,0 +1,24 @@
+package go_structor
+
+import (
+	"github.com/goreflect/go_structor/pipeline"
+)
+
+// ConfigureEasy - default pipeline setup for configure your structure
+func ConfigureEasy(
+	structure interface{},
+	fileName string) error {
+	return pipeline.Configure(structure, fileName, []pipeline.FuncType{
+		pipeline.FunctionSetupEnvironment,
+		pipeline.FunctionSetupHocon,
+		pipeline.FunctionSetupDefault,
+	})
+}
+
+// ConfigureSetup - pipeline with your settings stages for your structure
+func ConfigureSetup(
+	structure interface{},
+	fileName string,
+	functions []pipeline.FuncType) error {
+	return pipeline.Configure(structure, fileName, functions)
+}
