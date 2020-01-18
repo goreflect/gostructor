23febd39d9bed9f27c2fadffbbc25602fe64193c
kubitre <kubitre@gmail.com>
2020-01-19 03:10:58 +0700

    add not a value type for understanding not setup value from source

---
+++ pipeline/not_value.go
@@ -0,0 +1,13 @@
+package pipeline
+
+// NotAValue - value for check setuping in fields
+type NotAValue struct {
+	ValueAddress interface{}
+}
+
+// NewNotAValue - create new not a value field interface for check true or false setuping value from source
+func NewNotAValue(field interface{}) *NotAValue {
+	return &NotAValue{
+		ValueAddress: field,
+	}
+}
