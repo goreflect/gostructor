7e11154193ed09a6639c28fa02ac8de8ec4b335b
kubitre <kubitre@gmail.com>
2020-04-05 19:21:19 +0700

    correct comments

--- pipeline/environment_function_test.go
+++ pipeline/environment_function_test.go
@@ -15,6 +15,7 @@ func TestEnvironmentConfig_GetBaseType(t *testing.T) {
 		field1 string `cf_env:"testBaseType"`
 	}{}
 	os.Setenv("testBaseType", "tururu")
+	defer os.Remove("testBaseType")
 	myStruct1 := reflect.Indirect(reflect.ValueOf(myStruct))
 	field1 := myStruct1.Type().Field(0)
 	t.Log("type of field: ", field1.Name+", "+field1.Type.Name())
@@ -96,6 +97,7 @@ func TestEnvironmentConfig_GetComplexType(t *testing.T) {
 		fieldhard []float32 `cf_env:"mySlice"`
 	}{}
 	os.Setenv("mySlice", "1.12,1.24,1.67")
+	defer os.Remove("mySlice")
 	myStruct1 := reflect.Indirect(reflect.ValueOf(myStruct))
 	field1 := myStruct1.Type().Field(0)
 	t.Log("type of field: ", field1.Name+", "+field1.Type.Name())
@@ -124,6 +126,7 @@ func TestEnvironmentConfig_GetComplexType(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			config := EnvironmentConfig{}
 			got := config.GetComplexType(tt.args.context)
+
 			if !reflect.DeepEqual(got.Value.Interface(), tt.want.Value.Interface()) {
 				t.Errorf("EnvironmentConfig.GetComplexType() = %v, want %v", got, tt.want)
 			}
