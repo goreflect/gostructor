4dc9a9ea6b838c4bb16e0dc9cb71accc8717c857
kubitre <kubitre@gmail.com>
2020-04-09 23:39:44 +0700

    update Readme by current version, and correct by comments

--- lib_test.go
+++ lib_test.go
@@ -48,7 +48,8 @@ type (
 func Test_parseHocon1(t *testing.T) {
 	myStruct, err := ConfigureSetup(&MyStruct{}, "./test_configs/test1.hocon", "", []infra.FuncType{infra.FunctionSetupHocon})
 	if err != nil {
-		t.Error("error while configuring: ", err)
+		t.Error("error while configurig: ", err)
+		return
 	}
 	assert.Equal(t, &MyStruct{
 		Field1: []string{"test1", "test2", "test3"},
@@ -62,6 +63,7 @@ func Test_parseHocon(t *testing.T) {
 	myStruct, err := ConfigureSetup(&MyStruct2{}, "./test_configs/testmap.hocon", "", []infra.FuncType{infra.FunctionSetupHocon})
 	if err != nil {
 		t.Error("error while configuring: ", err)
+		return
 	}
 	assert.Equal(t, &MyStruct2{
 		NestedStruct1: NestedStruct1{
@@ -131,7 +133,8 @@ func Test_getValueFromEnvironment(t *testing.T) {
 	}()
 	myStruct, err := ConfigureSmart(&EnvStruct{}, "")
 	if err != nil {
-		fmt.Println("error while configuring: ", err)
+		t.Error(err)
+		return
 	}
 	assert.Equal(t, &MyStruct4{
 		NestedStruct4: struct{ Field1 string }{
@@ -155,9 +158,17 @@ func Test_configureEasy(t *testing.T) {
 	os.Setenv("myField3", "true")
 	os.Setenv("myField4", "12.2")
 	os.Setenv("myField5", "true,false,true")
+	defer func() {
+		os.Unsetenv("myField1")
+		os.Unsetenv("myField2")
+		os.Unsetenv("myField3")
+		os.Unsetenv("myField4")
+		os.Unsetenv("myField5")
+	}()
 	myStruct, err := ConfigureEasy(&EnvStruct{}, "")
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
 	assert.Equal(t, &EnvStruct{
@@ -167,9 +178,5 @@ func Test_configureEasy(t *testing.T) {
 		Field4: 12.2,
 		Field5: []bool{true, false, true},
 	}, myStruct.(*EnvStruct))
-	os.Remove("myField1")
-	os.Remove("myField2")
-	os.Remove("myField3")
-	os.Remove("myField4")
-	os.Remove("myField5")
+
 }
